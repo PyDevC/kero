67d4254cfff3023f8bf1044c0cdbe91fdc0a14e7
PyDevC <anuskcse@gmail.com>
2025-04-04 18:40:04 +0530

    added base components of each kero-data-type

--- kero/base_tensor.py
+++ kero/base_tensor.py
@@ -5,22 +5,27 @@ import torch
 
 # The below are classes in which each tensor will be represented
 
-class TensorTable:
+class TensorTable: # need some time think about its development
     def __init__(self):
-        pass
+        self.tensor = None
+        self.dim = ()
 
 class NumTensor:
     def __init__(self):
-        pass
+        self.tensor = None
+        self.dim = ()
 
 class StrTensor:
     def __init__(self):
-        pass
+        self.tensor = None 
+        self.dim = ()
 
 class ScalerTensor:
     def __init__(self):
-        pass
+        self.tensor = None 
+        self.dim = ()
 
 class DateTensor:
     def __init__(self):
-        pass
+        self.tensor = None 
+        self.dim = ()
