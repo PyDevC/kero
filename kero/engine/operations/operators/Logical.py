ee44cd9201c60bec4155ff430620b6326154fdfc
PyDevC <anuskcse@gmail.com>
2025-04-08 23:50:07 +0530

    fixed logical operations

--- kero/engine/operations/operators/Logical.py
+++ kero/engine/operations/operators/Logical.py
@@ -7,32 +7,37 @@ class logical_op(ABC, operator):
         super().__init__(optype)
         self.operand1 = tensor1
         self.operand2 = tensor2
+        self._check_type()
+
+    def _check_type(self):
+        if isinstance(self.operand2, int):
+            self.operand2 = torch.tensor(self.operand2)
 
 class eq(logical_op):
     def execute(self):
-        return torch.eq(self.operand1, self.operand2)
+        return torch.equal(self.operand1, self.operand2)
 
 
 class ne(logical_op):
     def execute(self):
-        return torch.ne(self.operand1, self.operand2)
+        return torch.not_equal(self.operand1, self.operand2)
 
 
 class gt(logical_op):
     def execute(self):
-        return torch.gt(self.operand1, self.operand2)
+        return torch.greater(self.operand1, self.operand2)
 
 
 class ge(logical_op):
     def execute(self):
-        return torch.ge(self.operand1, self.operand2)
+        return torch.greater_equal(self.operand1, self.operand2)
 
 
 class lt(logical_op):
     def execute(self):
-        return torch.lt(self.operand1, self.operand2)
+        return torch.less(self.operand1, self.operand2)
 
 
 class le(logical_op):
     def execute(self):
-        return torch.le(self.operand1, self.operand2)
+        return torch.less_equal(self.operand1, self.operand2)
--- kero/engine/operations/operators/base.py
+++ kero/engine/operations/operators/base.py
@@ -5,7 +5,3 @@ class operator():
     def __init__(self, optype):
         self.opname = self.__class__.__name__
         self.optype = optype
-
-    @abstractmethod
-    def execute(self) -> torch.Tensor:
-        pass
