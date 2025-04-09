9dafede2cec1343488883aeb560c3935e036da13
PyDevC <anuskcse@gmail.com>
2025-04-09 19:40:07 +0530

    changed device

--- example_final.py
+++ example_final.py
@@ -1,13 +1,14 @@
 import torch
+import pandas as pd
 from kero import TableTensor, NumTensor
 from kero.engine import Executor, Parser
 import numpy as np
 
-id = np.random.randint(1 ,10 , size=(100000000,)) # 10 million queries
-age = np.random.randint(18 ,100 , size=(100000000,)) # 10 million queries
+id = np.random.randint(1 ,10 , size=(10000000,)) # 10 million queries
+age = np.random.randint(18 ,100 , size=(10000000,)) # 10 million queries
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
+# This is how we call 10 million rows and get them queried
+device = 'cpu'
 table_data = {
     "employee_id": NumTensor(torch.from_numpy(id), name="employee_id"),
     "age": NumTensor(torch.from_numpy(age), name="age"),
@@ -18,7 +19,7 @@ table_tensor = TableTensor(columns=table_data, name="employees")
 table_tensor.to(device)
 
 query = """
-SELECT employee_id, age, int, col
+SELECT employee_id, age
 FROM employees 
 WHERE employee_id > 7; 
 """
@@ -30,4 +31,8 @@ kquery = parser.parse(query)
 executor = Executor(table_tensor)
 
 result = executor.execute(kquery)
-print(result)
+
+result = pd.DataFrame(result)
+shape = result.shape
+pd.set_option('display.max_rows', shape[0])
+print(result.head(shape[0]))
