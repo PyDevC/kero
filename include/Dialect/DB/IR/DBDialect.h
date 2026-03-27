163a6491ed5e9855ee07b6c5c28db54c5ac5fbc1
PyDevC <anuskcse@gmail.com>
2026-03-27 10:35:33 +0000

    Created new DBDialect empty dialect with essential compilation configs
    Each Directory has it's own CMakeLists.txt file.
    Since Dialect is empty, the initialize function just retuns void for now

---
+++ include/Dialect/DB/IR/DBDialect.h
@@ -0,0 +1,8 @@
+#ifndef DIALECT_DB_IR_DBDIALECT_H_
+#define DIALECT_DB_IR_DBDIALECT_H_
+
+#include "mlir/IR/DialectImplementation.h"
+
+#include "Dialect/DB/IR/DBDialect.h.inc"
+
+#endif // DIALECT_DB_IR_DBDIALECT_H_
---
+++ lib/Dialect/DB/DBDialect.cpp
@@ -0,0 +1,10 @@
+#include "Dialect/DB/IR/DBDialect.h"
+#include "Dialect/DB/IR/DBDialect.cpp.inc"
+
+namespace mlir {
+namespace db {
+
+void DBDialect::initialize() { return; };
+
+} // namespace db
+} // namespace mlir
