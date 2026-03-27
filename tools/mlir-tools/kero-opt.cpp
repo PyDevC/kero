8d168ac537f751222e6e9b6035d8588a6e45df13
PyDevC <anuskcse@gmail.com>
2026-03-27 14:31:24 +0000

    tools: Added FuncDialect to support func.func in .mlir tests

--- tools/mlir-tools/kero-opt.cpp
+++ tools/mlir-tools/kero-opt.cpp
@@ -2,11 +2,13 @@
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
 #include "Dialect/DB/IR/DBDialect.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
-int main(int argc, char **argv) {
-  mlir::DialectRegistry registry;
-  registry.insert<mlir::db::DBDialect>();
+int main(int argc, char** argv) {
+    mlir::DialectRegistry registry;
+    registry.insert<mlir::db::DBDialect>();
+    registry.insert<mlir::func::FuncDialect>();
 
-  return mlir::asMainReturnCode(
-      mlir::MlirOptMain(argc, argv, "Kero-binary", registry));
+    return mlir::asMainReturnCode(
+        mlir::MlirOptMain(argc, argv, "Kero-binary", registry));
 }
