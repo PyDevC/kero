896a6711f0b4a25ca2897acf14ba9b2bf9bf33b6
PyDevC <anuskcse@gmail.com>
2026-03-27 12:45:06 +0000

    Added TableType and ColumnType in db namespace for DBDialect

--- include/Dialect/DB/IR/DBDialect.h
+++ include/Dialect/DB/IR/DBDialect.h
@@ -2,6 +2,7 @@
 #define DIALECT_DB_IR_DBDIALECT_H_
 
 #include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/OpDefinition.h"
 
 #include "Dialect/DB/IR/DBDialect.h.inc"
 
--- lib/Dialect/DB/DBDialect.cpp
+++ lib/Dialect/DB/DBDialect.cpp
@@ -1,18 +1,24 @@
 #include "Dialect/DB/IR/DBDialect.h"
+#include "Dialect/DB/IR/DBTypes.h"
+
+#include "llvm/ADT/TypeSwitch.h"
+#include "mlir/IR/Builders.h"
+#include <string>
+
+#include "Dialect/DB/IR/DBDialect.cpp.inc"
 
 #define GET_TYPEDEF_CLASSES
-#include "Dialect/DB/IR/DBDialect.cpp.inc"
+#include "Dialect/DB/IR/DBDialectType.cpp.inc"
 
 namespace mlir {
 namespace db {
 
 void DBDialect::initialize() {
-
+   addTypes<
 #define GET_TYPEDEF_LIST
-  addTypes<
-#include "Dialect/DB/IR/DBDialect.cpp.inc"
-      >;
-};
+#include "Dialect/DB/IR/DBDialectType.cpp.inc"
+      >();
+}
 
 } // namespace db
 } // namespace mlir
