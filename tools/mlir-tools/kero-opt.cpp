5e4ebb993a9241ea9e2c68a01005ad90cedaf5da
PyDevC <anuskcse@gmail.com>
2026-03-29 11:00:48 +0000

    DB: Added return, filter, and getcol operations for db dialect

--- tools/mlir-tools/kero-opt.cpp
+++ tools/mlir-tools/kero-opt.cpp
@@ -3,11 +3,13 @@
 
 #include "Dialect/DB/IR/DBDialect.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
 
 int main(int argc, char** argv) {
     mlir::DialectRegistry registry;
     registry.insert<mlir::db::DBDialect>();
     registry.insert<mlir::func::FuncDialect>();
+    registry.insert<mlir::arith::ArithDialect>();
 
     return mlir::asMainReturnCode(
         mlir::MlirOptMain(argc, argv, "Kero-binary", registry));
