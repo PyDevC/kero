cdd2161684cfdde9622e8bf4a87590e7fec45a6f
PyDevC <anuskcse@gmail.com>
2026-04-16 17:25:48 +0000

    old dead code removal

--- kero/engine/__init__.py
+++ kero/engine/__init__.py
@@ -1,13 +1,7 @@
-from .operations import operators
 from .compiler import KeroCompiler
 from .executor import Executor
-from .queryparser import Parser
-from .arrow import ArrowHandler # Add this line
 
 __all__ = [
     "Executor",
     "KeroCompiler",
-    "Parser",
-    "operators",
-    "ArrowHandler",
 ]
