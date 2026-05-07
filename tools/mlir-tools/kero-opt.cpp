8f801884fc992a5ddea1bef10574c9a449afa249
PyDevC <anuskcse@gmail.com>
2026-05-07 10:53:05 +0000

    kero-opt: register all dialects and passes with llvm lowering

--- tools/mlir-tools/kero-opt.cpp
+++ tools/mlir-tools/kero-opt.cpp
@@ -1,23 +1,21 @@
 #include "Conversion/DBToTensor/DBToTensor.h"
 #include "Dialect/DB/IR/DBDialect.h"
 
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/Linalg/IR/Linalg.h"
-#include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/Conversion/ConvertToLLVM/ToLLVMPass.h"
 #include "mlir/IR/Dialect.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
 int main(int argc, char** argv) {
     mlir::DialectRegistry registry;
+    
+    mlir::registerAllDialects(registry);
     registry.insert<mlir::db::DBDialect>();
-    registry.insert<mlir::arith::ArithDialect>();
-    registry.insert<mlir::func::FuncDialect>();
-    registry.insert<mlir::linalg::LinalgDialect>();
-    registry.insert<mlir::scf::SCFDialect>();
-    registry.insert<mlir::tensor::TensorDialect>();
 
+    mlir::registerAllPasses();
+    mlir::registerConvertToLLVMDependentDialectLoading(registry);
+    
     mlir::db::registerDBToTensorPasses();
 
     return mlir::asMainReturnCode(
