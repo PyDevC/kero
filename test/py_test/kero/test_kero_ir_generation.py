828a18fc4873edcbeb0dbf6d837272ffe15e4c3a
PyDevC <anuskcse@gmail.com>
2026-05-28 11:31:56 +0000

    fix: add_compile_definition path from _kero. to kero._engine._kero
    testing ir generation with scan op and all types

--- test/py_test/kero/test_kero_ir_generation.py
+++ test/py_test/kero/test_kero_ir_generation.py
@@ -1,29 +1,40 @@
 from unittest import TestCase
-from kero._engine._kero.dialects import db
-from kero._engine._kero.ir import (
-    Context,
-    Module,
-    InsertionPoint,
-    Location,
-    Type,
-    Block
-)
 
 from kero._engine._kero._mlir_libs import _keroEngine
+from kero._engine._kero.dialects import db, func
+import kero._engine._kero.ir as ir
 
 class TestOperationGeneration(TestCase):
 
     def setUp(self):
-        self.ctx = Context()
+        self.ctx = ir.Context()
         _keroEngine.register_dialect(self.ctx)
-        self.loc = Location.unknown(self.ctx)
-
-    def _make_mod(self):
-        return Module.create(self.loc)
+        self.loc = ir.Location.unknown(self.ctx)
+        self.module = ir.Module.create(loc=self.loc)
+        self.func = None
 
     def test_db_types(self):
-        mod = self._make_mod()
-        table_t = Type.parse('!db.table<"user", 5, 1000>', self.ctx)
-        result_t = Type.parse('!db.result', self.ctx)
-        assert table_t
-        assert result_t
+        with self.ctx, self.loc:
+            table_t = ir.Type.parse('!db.table<"user", 5, 1000>')
+            column_t = ir.Type.parse(f'!db.column<"user", "age", i32, 1000>')
+            result_t = ir.Type.parse('!db.result')
+
+            assert isinstance(table_t, ir.Type)
+            assert isinstance(column_t, ir.Type)
+            assert isinstance(result_t, ir.Type)
+
+    def test_scan_op(self):
+        with self.ctx, self.loc:
+            with ir.InsertionPoint(self.module.body):
+                table_t = ir.Type.parse('!db.table<"user", 5, 1000>')
+                result_t = ir.Type.parse('!db.result')
+
+
+                @func.FuncOp.from_py_func(table_t)
+                def test_scan_op_func(arg0):
+                    scanop = db.scan(
+                        result=result_t,
+                        table=arg0,
+                    )
+
+                    return scanop
