c169f6ec09043032b7cc77b40ede2ef459fad211
PyDevC <anuskcse@gmail.com>
2026-06-08 10:22:40 +0000

    kero: outputOp verifier to correctness of select attribute and column attribute
    contains two verification:
    - verify if the select column exists in the table type
    - verify if the output table only has the selected columns

--- lib/Dialect/DB/DBDialect.cpp
+++ lib/Dialect/DB/DBDialect.cpp
@@ -37,6 +37,9 @@ void DBDialect::initialize() {
         >();
 }
 
+//===----------------------------------------------------------------------===//
+// Type Verifiers
+//===----------------------------------------------------------------------===//
 llvm::LogicalResult TableType::verify(
     ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
     ::llvm::ArrayRef<::mlir::db::ColumnAttr> columns,
@@ -54,8 +57,64 @@ llvm::LogicalResult TableType::verify(
     for (auto column : columns) {
         auto colNrows = column.getNrows();
         if (colNrows != nrows) {
-            return emitError() << "nrows in column attribute " 
-            << column.getName() << " is not equal to nrows in whole table";
+            return emitError() << "nrows in column attribute "
+                               << column.getName()
+                               << " is not equal to nrows in whole table";
+        }
+    }
+
+    return llvm::success();
+}
+
+//===----------------------------------------------------------------------===//
+// Operation Verifiers
+//===----------------------------------------------------------------------===//
+llvm::LogicalResult OutputOp::verify() {
+    auto tableType = mlir::dyn_cast_or_null<TableType>(getOperand().getType());
+    if (!tableType) {
+        return emitError() << "Expected a operand to be of table type";
+    }
+
+    auto selectAttrArray = getSelectAttr();
+    auto columns = tableType.getColumns();
+
+    // Verify if elements in selectAttrArray are present in tableType
+    for (auto selectAttr : selectAttrArray) {
+        auto stringAttr = mlir::dyn_cast<mlir::StringAttr>(selectAttr);
+        auto columnName = stringAttr.getValue();
+        auto it = std::find_if(columns.begin(),
+                               columns.end(),
+                               [&](const mlir::db::ColumnAttr col) {
+                                   return col.getName() == columnName;
+                               });
+
+        if (it == columns.end()) {
+            return emitError() << "select column " << selectAttr
+                               << " is not present in the table";
+        }
+    }
+
+    // Verify if output contains only selected columns or not
+    auto output = mlir::dyn_cast<mlir::db::TableType>(getResult().getType());
+    auto outputColumns = output.getColumns();
+
+    if (outputColumns.size() != selectAttrArray.size()) {
+        return emitError() << "number of selected columns does not match number"
+                              " of column attributes in output table";
+    }
+
+    for (auto outColumn : outputColumns) {
+        auto outColumnName = outColumn.getName();
+        auto it = std::find_if(selectAttrArray.begin(),
+                               selectAttrArray.end(),
+                               [&](mlir::Attribute selectAttr) {
+                                   auto attr = mlir::dyn_cast<mlir::StringAttr>(selectAttr);
+                                   return attr.getValue() == outColumnName;
+                               });
+
+        if (it == selectAttrArray.end()) {
+            return emitError() << "output column " << outColumnName
+                               << " was not specified in the selected attributes";
         }
     }
 
