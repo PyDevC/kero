cd5df541b06e3e032dce1ab66b518843f5a60142
PyDevC <anuskcse@gmail.com>
2026-06-12 09:32:35 +0000

    kero: generate opeartions list from the parser used by IR gen

--- kero/engine/parser.py
+++ kero/engine/parser.py
@@ -1,36 +1,47 @@
 """Parser for SQLGlot AST to DB AST"""
-# NOTE: It is incomplete as of now, we need to add support for operations
 
 import sqlglot
 import sqlglot.expressions as expr
 
-from ..arrow import type_resolve as arrow_ast
+from ..arrow import type_resolve as resolve
 from ..arrow.data import Dataset
 
 import typing as t
 
-
-class ScanOp:
-    def __init__(self, tables: t.List[arrow_ast.Table]):
-        self.scan_tables: t.List[arrow_ast.Table] = tables
-
-    def __repr__(self) -> str:
-        return f"db.scan [{self.scan_tables}]"
-
-class ProjectOp:
-    def __init__(self, projected):
-        self.projected: t.List[arrow_ast.Column] = projected
+# TODO(PyDevC): Format the code to make it more readable, everything is cluttered right now
+class ScanOp(resolve.DBOps):
+    def __init__(self, input_table: resolve.Table, outputs=None, attribute=None):
+        super().__init__("scan", outputs=outputs, attribute=attribute)
+        self.input_table: resolve.Table = input_table
 
     def __repr__(self) -> str:
-        return f"db.project [{self.projected}]"
+        # Keep the input table and output table same
+        if self.attribute is not None:
+            return f'db.scan {{ {self.attribute["table_name"]} }} : {self.input_table} -> {self.input_table}'
+
+        return f'db.scan : {self.input_table} -> {self.input_table}'
+
+class OutputOp(resolve.DBOps):
+    def __init__(self, input_table: resolve.Table, output_table: resolve.Table, attribute=None):
+        super().__init__("output", attribute=attribute)
+        self.output_table = output_table
+        self.input_table = input_table
+
+    def __repr__(self) -> str:
+        if self.attribute is not None:
+            return f'db.output {{ select = [{self.attribute["select"]}] }} : {self.input_table} -> {self.output_table}'
+        else:
+            # TODO(PyDevC): raise Exception that no column was selected
+            raise Exception()
 
 
 class ParserExecption(Exception): ...
+class ParserNodeNotFound(ParserExecption): ...
 
 
 class Parser:
     def __init__(self, dataset: Dataset):
-        self.ast = {}
+        self.operations = []
         self.glot_ast: sqlglot.Expr
         self.dataset = dataset
 
@@ -39,47 +50,60 @@ class Parser:
 
         root = self.glot_ast.find(expr.Select)
         if not root:
-            raise ParserExecption("Only Select statements are supported")
-
+            raise ParserExecption(
+                f"Only Select statements are supported\n",
+                f"Glot AST Trace: ",
+                repr(self.glot_ast)
+            )
 
         root_tables = self._get_tables(root)
         tables = []
 
         for table in root_tables:
-            table_node = arrow_ast.Table(table.name, {}, 0, 0)
+            table_node = resolve.Table(table.name, {}, 0, 0)
             dataset_table = self.dataset.get_table(table.name)
-            arrow_ast.resolve_table(dataset_table, table_node)
+            resolve.resolve_table(dataset_table, table_node)
             tables.append(table_node)
 
+        dag_node_scan = ScanOp(tables[0], attribute=None)
+        self.operations.append(dag_node_scan)
+
         selected_columns = self._get_columns(root)
-        columns = []
+        columns = {}
 
         for column in selected_columns:
             # having table_node unbounded is not a problem 
             # will get error before we even reach here
             column_node = table_node.columns.get(column.name)
             if column_node:
-                columns.append(column_node)
+                columns[column.name] = column_node
 
-        self.add_node(ScanOp(tables), "db.scan")
-        self.add_node(ProjectOp(columns), "db.project")
 
-        return self.ast
+        selected_table = resolve.Table("temp", columns, len(columns), table_node.nrows)
 
-    def walk(self) -> t.Iterator:
-        """return the iterator for walking the ast nodes in order"""
-        return iter(self.ast)
+        dag_node_output = OutputOp(input_table=dag_node_scan.input_table, output_table=selected_table, attribute={"select": list(columns.values())})
+        self.operations.append(dag_node_output)
+
+        return self.operations
 
     def _get_columns(self, node: expr.Select) -> t.List[expr.Column]:
-        selected_columns = node.find_all(expr.Column)
+        # TODO (PyDevC): This is will not work for some complex cases
+        # Change how to extract the columns from the node
+        selected_columns = node.args.get("expressions")
         return list(selected_columns)
 
     def _get_tables(self, node: expr.Select) -> t.List[expr.Table]:
-        tables = node.find_all(expr.Table)
-        return list(tables)
+        from_node = node.args.get("from_")
+        if from_node is None:
+            raise ParserNodeNotFound(
+                "Glot does not have FROM node",
+                repr(node)
+            )
+        tables = from_node.this
 
-    def add_node(self, node, key):
-        self.ast[key] = node
+        return [tables]
 
-    def __repr__(self) -> str:
-        return str(self.ast)
+    def _get_where(self, node: expr.Select):
+        where_node = node.args.get("where")
+        # TODO (PyDevC): Complete this when working with filter op
+        pass
