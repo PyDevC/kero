ad9cad818696157393ab44c728e6a0f4914bb0f0
PyDevC <anuskcse@gmail.com>
2026-07-02 09:12:28 +0000

    kero: make torch optional by adding try import torch

--- kero/__init__.py
+++ kero/__init__.py
@@ -1,18 +1,23 @@
-from .tensors import (
-    DateTensor,
-    NumTensor,
-    ScalarTensor,
-    StrTensor,
-    TableTensor
-)
-
-__all__ = [
-    "DateTensor",
-    "NumTensor",
-    "ScalarTensor",
-    "StrTensor",
-    "TableTensor",
-]
-
-# Please keep this list sorted
-assert __all__ == sorted(__all__)
+try:
+    import torch
+except ImportError:
+    __all__ = []
+else:
+    from .tensors import (
+        DateTensor,
+        NumTensor,
+        ScalarTensor,
+        StrTensor,
+        TableTensor
+    )
+    
+    __all__ = [
+        "DateTensor",
+        "NumTensor",
+        "ScalarTensor",
+        "StrTensor",
+        "TableTensor",
+    ]
+    
+    # Please keep this list sorted
+    assert __all__ == sorted(__all__)
